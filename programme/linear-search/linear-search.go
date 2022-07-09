a07f96ba3da1d6c883dbbee43d7d30ba1e537fed
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-09 12:34:25 +0530

    Update linear-search.go

--- programme/linear-search/linear-search.go
+++ programme/linear-search/linear-search.go
@@ -1,33 +1,33 @@
-// Write a GO programme for Linear Search.
+// Write a programme for Linear Search
 package main
 
 import "fmt"
 
-func main () {
+func main() {
 	var n, i, value, target int
-	fmt.Printf("Enter number of elements in the array: ")
+	fmt.Printf("Enter number of elements in the array : ")
 	fmt.Scan(&n)
 	var array []int
 	fmt.Printf("Enter %d elements : \n", n)
-	for i = 0 ; i < n ; i++ {
+	for i = 0; i < n; i++ {
 		fmt.Scan(&value)
 		array = append(array, value)
 	}
 
-	fmt.Printf("Enter the value to search in the array: ")
+	fmt.Printf("\nEnter the value to search in the array : ")
 	fmt.Scan(&target)
 
 	value = -1
-	for i = 0 ; i < n ; i++ {
-		if target == array[i]{
+	for i = 0; i < n; i++ {
+		if target == array[i] {
 			value = i
 			break
 		}
 	}
 
 	if value == -1 {
-		fmt.Printf("%d does not exists in the given array.\n", target)
+		fmt.Printf("\n%d does not exists in the given array\n", target)
 	} else {
-		fmt.Printf("%d is located at index position %d .\n", target, value)
+		fmt.Printf("\n%d is located at index position %d\n", target, value)
 	}
 }
