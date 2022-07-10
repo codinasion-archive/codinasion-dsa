987d59891311359f0d28f8b8f88951815a703b12
PaoloFer <paolo.ferrari.vr.05@gmail.com>
2022-07-10 16:25:16 +0200

    python linear search

---
+++ programme/linear-search/linear-search.py
@@ -0,0 +1,15 @@
+# Write a programme for Linear Search
+
+def linear_search(arr, target):
+
+    for i in range(len(arr)):
+ 
+        if arr[i] == target:
+            print("Target found at index: ", i)
+            return i
+    print("Target not found")
+    return None
+arr=[10, 20, 80 ,30, 60, 50]
+target=30
+
+linear_search(arr,target)
