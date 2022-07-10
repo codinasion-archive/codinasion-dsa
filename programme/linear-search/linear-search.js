dac93672bf9f670b57ff3df03cdc963d4be88426
PaoloFer <paolo.ferrari.vr.05@gmail.com>
2022-07-10 16:41:35 +0200

    javascript linear search

---
+++ programme/linear-search/linear-search.js
@@ -0,0 +1,20 @@
+//Write a programme for Linear Search in Javascript
+
+function linearSearch(arr, item) {
+    // Go through all the elements of arr to look for item.
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) { // Found it!
+        console.log("Target found at index:", i)
+        return i;
+      }
+    }
+    
+    // Item not found in the array.
+    console.log("Target not found")
+    return null;
+}
+
+const arr = [10, 20, 80 ,30, 60, 50];
+let target=30;
+
+linearSearch(arr,target)
