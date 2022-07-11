3d4ee79b2e4ca169ddb9873ed354fbe2cb5c368e
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-11 08:28:18 +0530

    Update linear-search.js

--- programme/linear-search/linear-search.js
+++ programme/linear-search/linear-search.js
@@ -1,20 +1,24 @@
 //Write a programme for Linear Search in Javascript
 
 function linearSearch(arr, item) {
-    // Go through all the elements of arr to look for item.
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === item) { // Found it!
-        console.log("Target found at index:", i)
-        return i;
-      }
+  // Go through all the elements of arr to look for item.
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      // Found it!
+      console.log("Target found at index:", i);
+      return i;
     }
-    
-    // Item not found in the array.
-    console.log("Target not found")
-    return null;
+  }
+
+  // Item not found in the array.
+  console.log("Target not found");
+  return null;
 }
 
-const arr = [10, 20, 80 ,30, 60, 50];
-let target=30;
+const arr = [10, 20, 80, 30, 60, 50];
+let target = 30;
 
-linearSearch(arr,target)
+console.log("List   : ", arr);
+console.log("Target : ", target, "\n");
+
+linearSearch(arr, target);
