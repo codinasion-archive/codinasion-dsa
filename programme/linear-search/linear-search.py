e24559910382164f6fb303ea0ab97f4208298f98
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-11 08:28:16 +0530

    Update linear-search.py

--- programme/linear-search/linear-search.py
+++ programme/linear-search/linear-search.py
@@ -3,13 +3,19 @@
 def linear_search(arr, target):
 
     for i in range(len(arr)):
- 
+
         if arr[i] == target:
             print("Target found at index: ", i)
             return i
     print("Target not found")
     return None
-arr=[10, 20, 80 ,30, 60, 50]
-target=30
 
-linear_search(arr,target)
+
+arr = [10, 20, 80, 30, 60, 50]
+target = 30
+
+print("List   :", arr)
+print("Target :", target)
+print()
+
+linear_search(arr, target)
