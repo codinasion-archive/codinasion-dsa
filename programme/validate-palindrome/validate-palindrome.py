335d5e6f1ce4c72373e0d5d5057948aa3294881a
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-13 19:42:04 +0530

    Update validate-palindrome.py

--- programme/validate-palindrome/validate-palindrome.py
+++ programme/validate-palindrome/validate-palindrome.py
@@ -1,16 +1,17 @@
-#Write a programme for Validate Palindrom word
+# Write a programme to Validate Palindrom string
 
 def validPalindrome(s):
-    lowString= s.lower()
-    
-    reverse_string =lowString[::-1]
-    #check if palindrom word
-    print(lowString ==reverse_string)
-    return s==reverse_string
- 
+    lowString = s.lower()
 
-example1 = "level"  
-example2 = "coding" 
+    reverse_string = lowString[::-1]
 
-validPalindrome(example1) #True
-validPalindrome(example2) #False
+    return s == reverse_string
+
+
+example1 = "level"
+print("Input  :", example1)
+print("Output :", validPalindrome(example1))
+
+example2 = "coding"
+print("\nInput  :", example2)
+print("Output :", validPalindrome(example2))
