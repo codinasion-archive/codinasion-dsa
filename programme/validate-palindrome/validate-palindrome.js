7ab525932b41e5b75d73a7cbfa873c42e243bfb8
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-13 19:42:01 +0530

    Update validate-palindrome.js

--- programme/validate-palindrome/validate-palindrome.js
+++ programme/validate-palindrome/validate-palindrome.js
@@ -1,14 +1,21 @@
-function palindrome(str) {
-   
-    var re = /[\W_]/g;
-    
-    var lowRegStr = str.toLowerCase().replace(re, '');
-       
-    var reverseStr = lowRegStr.split('').reverse().join(''); 
+// Write a programme to Validate Palindrom string
 
-    console.log(reverseStr === lowRegStr);
-    return reverseStr === lowRegStr; 
-  }
-   
-palindrome("level"); //true
-palindrome("coding"); //false
+function palindrome(str) {
+  var re = /[\W_]/g;
+
+  var lowRegStr = str.toLowerCase().replace(re, "");
+
+  var reverseStr = lowRegStr.split("").reverse().join("");
+
+  return reverseStr === lowRegStr;
+}
+
+const str1 = "level";
+console.log("Input  : " + str1);
+console.log("Output : " + palindrome(str1));
+
+console.log("\n");
+
+const str2 = "coding";
+console.log("Input  : " + str2);
+console.log("Output : " + palindrome(str2));
