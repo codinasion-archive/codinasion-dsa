bc528c114f1861e7d488a0f2ca7a2f6e7f059238
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 15:51:32 +0530

    Update validate-palindrome.go

--- programme/validate-palindrome/validate-palindrome.go
+++ programme/validate-palindrome/validate-palindrome.go
@@ -1,3 +1,4 @@
+// Write a programme to validate palindrome
 package main
 
 import (
@@ -6,9 +7,9 @@ import (
 
 func main() {
 	var str string
-	fmt.Println("Enter a string: ")
+	fmt.Print("Enter a string : ")
 	fmt.Scan(&str)
-	fmt.Println("Is", str, "a palindrome: ", isPalindrome(str))
+	fmt.Println("\nIs '"+str+"' a palindrome : ", isPalindrome(str))
 }
 
 func isPalindrome(str string) bool {
