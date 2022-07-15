31a0e6ac05b053a985662a0a2e7a9f989960c722
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 19:15:36 +0530

    Update validate-palindrome.cpp

--- programme/validate-palindrome/validate-palindrome.cpp
+++ programme/validate-palindrome/validate-palindrome.cpp
@@ -1,55 +1,52 @@
 // Write a programme to Validate Palindrome string
-
 #include <string>
 #include <iostream>
+
 using namespace std;
 
-string validPalindrome(string s){
+string validPalindrome(string s)
+{
 
     // Automatically returns true if the string is empty
-    if (s.length() == 0){
+    if (s.length() == 0)
+    {
         return "True";
     }
 
     int i = 0;
-    
+
     // Turns all cases of the string to lower case
-    while (i < s.length()){
+    while (i < s.length())
+    {
         s[i] = tolower(s[i]);
         i++;
     }
-    
+
     string reverse = "";
 
     i = s.length() - 1;
-    while(reverse.length() != s.length()){
+    while (reverse.length() != s.length())
+    {
         reverse.push_back(s[i]);
         i--;
     }
 
-    if (reverse.compare(s) == 0){
+    if (reverse.compare(s) == 0)
+    {
         return "True";
-    } else {
+    }
+    else
+    {
         return "False";
     }
-    
 }
 
+int main()
+{
+    string i;
+    cout << "Input  : ";
+    cin >> i;
+    cout << "Output : " << validPalindrome(i) << endl;
 
-int main(){
-
-    string i = "TacoCat";
-
-    cout << i << ": " << validPalindrome(i) << endl;
-
-    i = "";
-
-    cout << i << ": " << validPalindrome(i) << endl;
-
-    i = "ToweR";
-
-    cout << i << ": " << validPalindrome(i) << endl;
-
-    
     return 0;
 }
