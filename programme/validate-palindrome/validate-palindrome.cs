5e80a2c879c1395d90d8dda1b93d1699ad835008
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-15 19:25:41 +0530

    Update validate-palindrome.cs

--- programme/validate-palindrome/validate-palindrome.cs
+++ programme/validate-palindrome/validate-palindrome.cs
@@ -1,27 +1,27 @@
+// Write a programme to validate palindrome
+using System;
+
 namespace validPalindrome
 {
     internal class Program
     {
+        static bool validPalindrome(string s)
+        {
+            string lowString = s.ToLower();
+
+            string reverseString = string.Empty;
+            for (int i = lowString.Length - 1; i >= 0; i--)
+            {
+                reverseString += lowString[i];
+            }
+            return s == reverseString;
+        }
+
         static void Main(string[] args)
         {
-            bool validPalindrome(string s)
-            {
-                string lowString = s.ToLower();
-
-                string reverseString = string.Empty;
-                for (int i = lowString.Length - 1; i >= 0; i--)
-                {
-                    reverseString += lowString[i];
-                }
-                return s == reverseString;
-            }
             string example1 = "level";
             Console.WriteLine($"Input  : {example1}");
             Console.WriteLine($"Output : {validPalindrome(example1)}");
-
-            string example2 = "coding";
-            Console.WriteLine($"\nInput  : {example2}");
-            Console.WriteLine($"Output : {validPalindrome(example2)}");
         }
     }
 }
