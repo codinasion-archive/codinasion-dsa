4049b04af8db936f94c4197b6263880825ba0c42
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-16 10:09:49 +0530

    Update binary-search.py

--- programme/binary-search/binary-search.py
+++ programme/binary-search/binary-search.py
@@ -1,31 +1,25 @@
-def binarySearch(arr, left, right, x):
-    if right >= left:
-        mid = left + (right - left) // 2
-  
-        if arr[mid] == x:
+# Write a programme for binary search
+
+def binarySearch(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
             return mid
-  
-        elif arr[mid] > x:
-            return binarySearch(arr, left, mid-1, x)
-  
+        elif arr[mid] < target:
+            low = mid + 1
         else:
-            return binarySearch(arr, mid + 1, right, x)
+            high = mid - 1
+    return -1
 
-    return None
 
+# get array from user
+arr = input("Enter array  : ").split()
+arr = [int(i) for i in arr]
+target = int(input("Enter target : "))
 
-print("Binary Search algorithm to find the position of an element in a sorted array.")
-
-while True:
-    try:
-        arr = list(map(int, input("\nInput: ").split(" ")))
-    except ValueError:
-        print("The array should only contain integers.")
-        continue
-
-    target = int(input("Target: "))
-    result = binarySearch(arr, 0, len(arr)-1, target)
-    if result:
-        print("Output:", result)
-    else:
-        print(f"Target {target} not found in the given array.")
+if binarySearch(arr, target) == -1:
+    print("\nOutput : Target not found")
+else:
+    print("\nOutput :", binarySearch(arr, target))
