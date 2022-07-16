11003f62831af4b5b0a22f4a63c519a7c5bdf0fe
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-16 19:40:36 +0530

    Update Program.cs

--- programme/binary-search/Program.cs
+++ programme/binary-search/Program.cs
@@ -1,58 +1,49 @@
-﻿//Binary Search
-
+// Write a programme for binary search
 using System;
 
-namespace BinarySearch
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[] example = new int[] { 10, 20, 30, 50, 60, 80 };
+        int target = 30;
+
+        string stringExample = printArray(example);
+        Console.WriteLine($"Input  : {stringExample}");
+        Console.WriteLine($"Target : {target}");
+        Console.WriteLine($"\nOutput : {BinarySearch(example, target)}");
+    }
+
+    public static string printArray(int[] array)
+    {
+        string stringExample = "";
+        for (int i = 0; i < array.Length; i++)
         {
-            int[] example = new int[] { 10, 20, 30, 50, 60, 80 };
-            int target = 30;
-
-            //Funciton for outputting our array into text
-            string stringExample = "";
-            void printArray(int[] array)
-            {
-                for (int i = 0; i < array.Length; i++)
-                {
-                    stringExample += $"{array[i]} ";
-                }
-            }
-
-            printArray(example);
-            Console.WriteLine($"Input : {stringExample}");
-            Console.WriteLine($"Target : {target}");
-
-            Console.WriteLine($"Output: {BinarySearch(example, target)}");
-
-
-
-            static object BinarySearch(int[] inputArray, int key)
-            {
-                int min = 0;
-                int max = inputArray.Length - 1;
-                while (min <= max)
-                {
-                    int mid = (min + max) / 2;
-                    if (key == inputArray[mid])
-                    {
-                        return mid++;
-                    }
-                    else if (key < inputArray[mid])
-                    {
-                        max = mid - 1;
-                    }
-                    else
-                    {
-                        min = mid + 1;
-                    }
-                }
-                return "Nil";
-            }
-
-            
+            stringExample += $"{array[i]} ";
         }
+        return stringExample;
+    }
+
+    public static object BinarySearch(int[] inputArray, int key)
+    {
+        int min = 0;
+        int max = inputArray.Length - 1;
+        while (min <= max)
+        {
+            int mid = (min + max) / 2;
+            if (key == inputArray[mid])
+            {
+                return mid++;
+            }
+            else if (key < inputArray[mid])
+            {
+                max = mid - 1;
+            }
+            else
+            {
+                min = mid + 1;
+            }
+        }
+        return "Nil";
     }
 }
