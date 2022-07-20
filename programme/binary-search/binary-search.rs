a0c4d2ab081ebb080bd89102f65ee9f19ba718be
SpirosArk <sp.arkoudelis@gmail.com>
2022-07-20 22:10:46 +0300

    rustBinarySearch

---
+++ programme/binary-search/binary-search.rs
@@ -0,0 +1,10 @@
+use core::cmp::Ordering;
+pub fn find<R: AsRef<[T]>, T: Ord>(space: R, key: T) -> Option<usize> {
+    let space = space.as_ref();
+    let mid = space.len() / 2;
+    match key.cmp(space.get(mid)?) {
+        Ordering::Equal => Some(mid),
+        Ordering::Less => find(&space[..mid], key),
+        Ordering::Greater => find(&space[mid + 1..], key).map(|i| i + mid + 1),
+    }
+}
