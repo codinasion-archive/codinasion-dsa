7b3d5e1676dce58eb4ec51da146639674ba7a62f
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-20 22:57:28 +0530

    Update binary-search.c

--- programme/binary-search/binary-search.c
+++ programme/binary-search/binary-search.c
@@ -1,10 +1,11 @@
-// Binary Search C code
-
+// Write a programme for binary search
 #include <stdio.h>
 
-int binarySearch(int array[], int x, int low, int high) {
+int binarySearch(int array[], int x, int low, int high)
+{
   // Repeat until the pointers low and high meet each other
-  while (low <= high) {
+  while (low <= high)
+  {
     int mid = low + (high - low) / 2;
 
     if (array[mid] == x)
@@ -20,14 +21,21 @@ int binarySearch(int array[], int x, int low, int high) {
   return -1;
 }
 
-int main(void) {
+int main(void)
+{
   int array[] = {10, 20, 30, 50, 60, 80};
   int n = sizeof(array) / sizeof(array[0]);
   int x = 30;
+  printf("Array  : ");
+  for (int i = 0; i < n; i++)
+    printf("%d ", array[i]);
+  printf("\nTarget : %d\n", x);
   int result = binarySearch(array, x, 0, n - 1);
+  printf("Output : ");
   if (result == -1)
     printf("Not found");
   else
     printf("%d", result);
+  printf("\n");
   return 0;
 }
