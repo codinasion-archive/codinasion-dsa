8e5bbf655f29decbbb061532d077a013bed8ebfc
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-20 22:50:03 +0530

    Update validate-palindrome.jl

--- programme/validate-palindrome/validate-palindrome.jl
+++ programme/validate-palindrome/validate-palindrome.jl
@@ -1,4 +1,10 @@
+# Write a programme to validate palindrome
+
 function palindrome(input)
-    reversed = join(reverse(split(input,"")),"")
+    reversed = join(reverse(split(input, "")), "")
     return input == reversed
 end
+
+input = "madam"
+println("Input  : ", input)
+println("Output : ", palindrome(input))
