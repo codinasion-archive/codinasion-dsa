5d882f074ae84865eb2235d4dae0d69095acab33
SpirosArk <sp.arkoudelis@gmail.com>
2022-07-20 22:05:07 +0300

    JuliaBinarySearch

---
+++ programme/binary-search/binary-search.jl
@@ -0,0 +1,22 @@
+function binary_search(list, query; rev = false, lt = <, by = identity)
+    if issorted(list) || issorted(list; rev=true)
+        low = !rev ? 1 : length(list)
+        high = !rev ? length(list) : 1
+        middle(l, h) = round(Int, (l + h)//2)
+        query = by(query)
+
+        while !rev ? low <= high : high <= low
+            mid = middle(low, high)
+            by(list[mid]) === query && return mid:mid
+            if lt(by(list[mid]), query)
+                low = !rev ? mid + 1 : mid - 1
+            else
+                high = !rev ? mid - 1 : mid + 1
+            end
+        end
+        return !rev ? (low:high) : (high:low)
+
+    else
+        throw(error("List not sorted, unable to search value"))
+    end
+end
