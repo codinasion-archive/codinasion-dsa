e210565998596ffede6eba353313794f85feb224
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-21 16:13:11 +0530

    Update binary-search.rs

--- programme/binary-search/binary-search.rs
+++ programme/binary-search/binary-search.rs
@@ -1,4 +1,6 @@
+// Write a programme for binary search
 use std::cmp::Ordering;
+
 pub fn find<R: AsRef<[T]>, T: Ord>(space: R, key: T) -> Option<usize> {
     let space = space.as_ref();
     let mid = space.len() / 2;
@@ -7,4 +9,12 @@ pub fn find<R: AsRef<[T]>, T: Ord>(space: R, key: T) -> Option<usize> {
         Ordering::Less => find(&space[..mid], key),
         Ordering::Greater => find(&space[mid + 1..], key).map(|i| i + mid + 1),
     }
+}
+
+pub fn main() {
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let target = 5;
+    println!("Array  : {:?}", array);
+    println!("Target : {}", target);
+    println!("Index  : {}", find(&array, target).unwrap());
 }
