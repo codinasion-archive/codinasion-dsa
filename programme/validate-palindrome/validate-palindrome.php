0c412288924410606091ec4a66247aa4742083bb
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-21 08:40:19 +0530

    Update validate-palindrome.php

--- programme/validate-palindrome/validate-palindrome.php
+++ programme/validate-palindrome/validate-palindrome.php
@@ -1,25 +1,30 @@
 <?php
-function Palindrome($MyString) {
-  $l = 0;
-  $r = strlen($MyString) - 1;
-  $flag = 0;
+// Write a programme to validate palindrome
 
-  while($r > $l){
-    if ($MyString[$l] != $MyString[$r]){
-      $flag = 1;
-      break;
+function Palindrome($MyString)
+{
+    $l = 0;
+    $r = strlen($MyString) - 1;
+    $flag = 0;
+
+    while ($r > $l) {
+        if ($MyString[$l] != $MyString[$r]) {
+            $flag = 1;
+            break;
+        }
+        $l++;
+        $r--;
     }
-    $l++;
-    $r--;
-  }
 
-  if ($flag == 0){
-    
-    echo "True\n";
-  } else {
-    echo "False\n";
-  }
+    if ($flag == 0) {
+
+        echo "True\n";
+    } else {
+        echo "False\n";
+    }
 }
 
-Palindrome("radar");
-?>
+$input = "radar";
+echo "Input  : " . $input . "\n";
+echo "Output : ";
+Palindrome($input);
