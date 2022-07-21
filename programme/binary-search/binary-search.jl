18e9b9e28818d3814808d87d37b96ff3fa1d58ee
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-21 08:51:36 +0530

    Update binary-search.jl

--- programme/binary-search/binary-search.jl
+++ programme/binary-search/binary-search.jl
@@ -1,8 +1,10 @@
-function binary_search(list, query; rev = false, lt = <, by = identity)
+# Write a programme for binary search
+
+function binary_search(list, query; rev=false, lt=<, by=identity)
     if issorted(list) || issorted(list; rev=true)
         low = !rev ? 1 : length(list)
         high = !rev ? length(list) : 1
-        middle(l, h) = round(Int, (l + h)//2)
+        middle(l, h) = round(Int, (l + h) // 2)
         query = by(query)
 
         while !rev ? low <= high : high <= low
@@ -20,3 +22,10 @@ function binary_search(list, query; rev = false, lt = <, by = identity)
         throw(error("List not sorted, unable to search value"))
     end
 end
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 9
+
+println("Input  : ", array)
+println("Target : ", target)
+println("Output : ", binary_search(array, target))
