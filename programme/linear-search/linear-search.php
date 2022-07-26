d5f362e1d65f08ca1e2d8d30f7f56ed0f7e1c339
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-26 17:16:49 +0530

    Update linear-search.php

--- programme/linear-search/linear-search.php
+++ programme/linear-search/linear-search.php
@@ -1,11 +1,18 @@
 <?php
-    function LinearSearch($arr, $x) {
-        for($i = 0; $i < sizeof($arr); $i++) {
-            if($arr[$i] == $x) return $i;
+// Write a programme for binary search
+function LinearSearch($arr, $x)
+{
+    for ($i = 0; $i < sizeof($arr); $i++) {
+        if ($arr[$i] == $x) {
+            return $i;
         }
-        return -1;
+
     }
-    $arr = array( 10, 20, 80, 30, 60, 50 );
-    $target = 30;
-    echo LinearSearch($arr, $target);
- ?>
+    return -1;
+}
+$arr = array(10, 20, 80, 30, 60, 50);
+$target = 30;
+echo "Array  : " . implode(" ", $arr) . "\n";
+echo "Target : $target\n";
+echo "Output : " . LinearSearch($arr, $target);
+echo "\n";
