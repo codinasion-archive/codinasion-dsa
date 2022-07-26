258ed4f8a51d5ec941a880988ab9a0b6681cb187
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-07-26 17:38:24 +0530

    Update BubbleSort.java

--- programme/bubble-sort/BubbleSort.java
+++ programme/bubble-sort/BubbleSort.java
@@ -1,26 +1,28 @@
-import java.util.ArrayList;
+// Write a programme for bubble sort
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        int[] arr = {5,4,1,6,2};
-        bubble(arr);
-        System.out.println(Arrays.toString(arr));
-    }
 
-    public static void bubblesort(int[] arr){
-        int sz = arr.length;
-        boolean swapped = false;
-        for(int i = 0; i < sz; i++){
-            for(int j = 1; j < sz - i; j++){
-                if(arr[j] < arr[j-1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j-1] = temp;
-                    swapped = true;
-                }
-            }
-            if(!swapped) break;
+  public static void main(String[] args) {
+    int[] arr = { 14, 33, 27, 35, 10 };
+    System.out.println("Input  : " + Arrays.toString(arr));
+    bubblesort(arr);
+    System.out.println("Output : " + Arrays.toString(arr));
+  }
+
+  public static void bubblesort(int[] arr) {
+    int sz = arr.length;
+    boolean swapped = false;
+    for (int i = 0; i < sz; i++) {
+      for (int j = 1; j < sz - i; j++) {
+        if (arr[j] < arr[j - 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+          swapped = true;
         }
+      }
+      if (!swapped) break;
     }
+  }
 }
