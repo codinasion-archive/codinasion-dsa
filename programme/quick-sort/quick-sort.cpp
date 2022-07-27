ae392c8c127681537c34c87a4e3f31b615ded578
Uday Badhe <74442221+udaybadhe@users.noreply.github.com>
2022-07-27 23:11:02 +0530

    Implemented Quick Sort in cpp

---
+++ programme/quick-sort/quick-sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+
+int partition(int array[], int low, int high) {
+    int pivot = array[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+    if (low < high) {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+
+int main() {
+    int array[] = {5,4,1,6,2};
+    int size = sizeof(array) / sizeof(array[0]);
+    quickSort(array, 0, size - 1);
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << ' ';
+    }
+}
