4bf9ba0fc1719653a69c7805ad3b0cc62c0cf2d7
Kacper Tomaszewki <casperxoxoholic@gmail.com>
2022-07-28 15:53:58 +0200

    Fixed typos and adjusted variable names

--- programme/quick-sort/quick-sort.cpp
+++ programme/quick-sort/quick-sort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 void swap(int *array, int index1, int index2);
-int partition(int *array, int lp, int rp, int pivot);
+int partition(int *array, int lowIndex, int highIndex, int pivot);
 void quickSort(int *array, int lowIndex, int highIndex);
 void printArray(int *array, int size);
 
@@ -28,14 +28,14 @@ void quickSort(int *array, int lowIndex, int highIndex) {
 	if (lowIndex >= highIndex)
 		return;
 	
-	// The pivot is always the last element
+	// the pivot is set to the last element
 	int pivot = array[highIndex];
 
-	int lp = partition(array, lowIndex, highIndex, pivot);
+	int pLeft = partition(array, lowIndex, highIndex, pivot);
 
 	// once the pointers have met, we sort the subarrays to the left and right side of the pivot
-	quickSort(array, lowIndex, lp - 1);
-	quickSort(array, lp + 1, highIndex);
+	quickSort(array, lowIndex, pLeft - 1);
+	quickSort(array, pLeft + 1, highIndex);
 }
 
 void swap(int *array, int index1, int index2) {
@@ -45,33 +45,33 @@ void swap(int *array, int index1, int index2) {
 }
 
 int partition(int *array, int lowIndex, int highIndex, int pivot) {
-	int lp = lowIndex;
-	int rp = highIndex - 1;
+	int pLeft = lowIndex;
+	int pRight = highIndex - 1;
 	
-	while (lp < rp) {
+	while (pLeft < pRight) {
 		// look for a left pointer, which is bigger than the pivot value
-		// breaks when the right pointer is reached as well
-		while (array[lp] <= pivot && lp < rp) {
-			lp++;
+		// breaks when the right pointer is met as well
+		while (array[pLeft] <= pivot && pLeft < pRight) {
+			pLeft++;
 		}
 		// look for a right pointer, which is smaller than the pivot value
-		// breaks when the left pointer is reached as well
-		while (array[rp] >= pivot && rp > lp) {
-			rp--;
+		// breaks when the left pointer is met as well
+		while (array[pRight] >= pivot && pRight > pLeft) {
+			pRight--;
 		}
 
-		// swap the elements, so that bigger elements are on the right side of the pivot and smaller one on the left
-		swap(array, lp, rp);
+		// swap the elements, so that bigger elements are on the right side of the pivot and the smaller ones on the left
+		swap(array, pLeft, pRight);
 	}
 
-	// this prevents the last element potentially being out of order
-	if (array[lp] > array[highIndex]) {
+	// this prevents the last element to potentially being out of order
+	if (array[pLeft] > array[highIndex]) {
 		// once the pointers meet, then we swap the element with the pivot
-		swap(array, lp, highIndex);
+		swap(array, pLeft, highIndex);
 	} else {
-		lp = highIndex;
+		pLeft = highIndex;
 	}
 
 	// at this point the pivot stands on the index where the left and right pointers met
-	return lp;
+	return pLeft;
 }
