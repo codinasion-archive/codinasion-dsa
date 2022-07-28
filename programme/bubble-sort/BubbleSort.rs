e2d2944d249d4684e97c02acd88c9c14d6856056
John Doe <79988159+John8790909@users.noreply.github.com>
2022-07-28 01:56:33 -0400

    Added Bubble Sort for Rust

---
+++ programme/bubble-sort/BubbleSort.rs
@@ -0,0 +1,32 @@
+fn bubble_sort(mut arr: Vec<i32>){
+    
+    let n = arr.len() - 1;
+    let mut sorted: bool = false;
+
+
+    while sorted == false{
+        sorted = true;
+        for i in 0..n{
+
+            if arr[i+1] < arr[i]{
+                sorted = false;
+                (arr[i], arr[i+1]) = (arr[i+1], arr[i]);
+            }
+        }
+
+    }
+
+    // this prints out the value of arr after bubble_sort 🤯
+    
+    println!("{:?}", arr)
+
+}
+
+// change this to pub main if used in a module
+fn main(){
+    let arr: Vec<i32> = vec![21, 4, 1, 3, 9, 8, 25, 0, 6, 2];
+    
+    // this prints out the value of arr before bubble_sort
+    println!("{:?}", arr);
+    bubble_sort(arr);
+}
