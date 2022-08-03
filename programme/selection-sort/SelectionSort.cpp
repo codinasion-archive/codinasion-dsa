297c5378ddeae0117fe298aa9dc5f8da3de6b1ba
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-08-03 08:35:55 +0530

    Update SelectionSort.cpp

--- programme/selection-sort/SelectionSort.cpp
+++ programme/selection-sort/SelectionSort.cpp
@@ -1,30 +1,41 @@
-// Write a programme for selection sort in cpp.
-#include<bits/stdc++.h>
+// Write a programme for selection sort
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
+int main()
+{
     // Selection sort Time complexity- O(n^2) , Space complexity- O(1).
-    int arr[]={5,4,1,6,2};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    cout<<"Before Selection Sort:"<<endl;
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    int arr[] = {5, 4, 1, 6, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Selection Sort : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
-    for(int i=0;i<n;i++){
-        int min_index=i;
-        for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[min_index]){  // comparing 
-                min_index=j;
+    cout << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            { // comparing
+                min_index = j;
             }
         }
-        int temp=arr[i];        // swapping minimum element from unsorted array with ith element.
-        arr[i]=arr[min_index];
-        arr[min_index]=temp;
+        int temp = arr[i]; // swapping minimum element from unsorted array with ith element.
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
     }
-    cout<<"After Selection Sort:"<<endl;
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+
+    cout << "After  Selection Sort : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
+    cout << endl;
+
     return 0;
 }
