5fb09f44dcbbadf7e8831f5d6c96aa8b19c8e28f
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2022-08-03 07:55:48 +0530

    Update BubbleSort.rs

--- programme/bubble-sort/BubbleSort.rs
+++ programme/bubble-sort/BubbleSort.rs
@@ -1,32 +1,28 @@
-fn bubble_sort(mut arr: Vec<i32>){
-    
+// Write a programme for bubble sort
+
+fn bubble_sort(mut arr: Vec<i32>) {
     let n = arr.len() - 1;
     let mut sorted: bool = false;
 
-
-    while sorted == false{
+    while sorted == false {
         sorted = true;
-        for i in 0..n{
-
-            if arr[i+1] < arr[i]{
+        for i in 0..n {
+            if arr[i + 1] < arr[i] {
                 sorted = false;
-                (arr[i], arr[i+1]) = (arr[i+1], arr[i]);
+                (arr[i], arr[i + 1]) = (arr[i + 1], arr[i]);
             }
         }
-
     }
 
     // this prints out the value of arr after bubble_sort 🤯
-    
-    println!("{:?}", arr)
-
+    println!("After  Sort : {:?}", arr)
 }
 
 // change this to pub main if used in a module
-fn main(){
+fn main() {
     let arr: Vec<i32> = vec![21, 4, 1, 3, 9, 8, 25, 0, 6, 2];
-    
+
     // this prints out the value of arr before bubble_sort
-    println!("{:?}", arr);
+    println!("Before Sort : {:?}", arr);
     bubble_sort(arr);
 }
